7fa6a15bb83e5336dd5ff774972a325fefafdf1a
Nicolas Broca <n.broca@gmail.com>
2023-09-06 17:55:10 +1000

    refactor prompt function

--- src/config/open-ai.ts
+++ src/config/open-ai.ts
@@ -1,6 +1,29 @@
 import 'dotenv/config'
 import OpenAI from "openai"
+import { Message } from 'discord.js'
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
+
+export async function getSystemMessages(message: Message): Promise<{ role: "function" | "user" | "system" | "assistant", content: string }[]> {
+  const systemMessages: { role: "function" | "user" | "system" | "assistant", content: string }[] = [
+    {
+      role: 'system',
+      content: `Je veux que tu comporte comme un ami à qui on demande un service. Je veux aussi que tu fasse des réponses de moins de 100 mots.`,
+    }, {
+      role: 'system',
+      content: 'Dans cette application ton nom est SoilAPT'
+    },
+  ]
+
+  if (message.reference) {
+    const originalMessage = await message.fetchReference()
+    systemMessages.push({
+      role: 'system',
+      content: `Tu répondras en sachant que l'utilisateur, ${message.author.displayName} a un ami qui s'appelle ${originalMessage.author.displayName}. ${message.author.displayName} vient te voir après que ${originalMessage.author.displayName} a dit "${originalMessage.content}".`
+    },)
+  }
+
+  return systemMessages
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { appendFile } from 'fs'
 
 import { Client, Events, GatewayIntentBits, Message } from 'discord.js'
-import { openai } from './config/open-ai'
+import { openai, getSystemMessages } from './config/open-ai'
 
 const { DISCORD_TOKEN } = process.env
 
@@ -24,46 +24,23 @@ async function run() {
 
 
     try {
-      if (message.reference) {
-        const originalMessage = await message.fetchReference()
-        const chatResponse = await openai.chat.completions.create({
-          messages: [
-            {
-              role: 'system',
-              content: `Je veux que tu comporte comme un ami à qui on demande un service. Je veux aussi que tu fasse des réponses de moins de 100 mots. Tu répondras en sachant que l'utilisateur, ${message.author.displayName} a un ami qui s'appelle ${originalMessage.author.displayName}. ${message.author.displayName} vient te voir après que ${originalMessage.author.displayName} a dit "${originalMessage.content}".`,
-            },
-            {
-              role: 'user',
-              content: message.content.replace(`<@${client.user.id}`, '')
-            }
-          ],
-          temperature: 0.7,
-          model: 'gpt-3.5-turbo'
-        })
+      const chatResponse = await openai.chat.completions.create({
+        messages: [
+          ...await getSystemMessages(message),
+          {
+            role: 'user',
+            content: message.content.replace(`<@${client.user.id}`, '').replace('@', '')
+          }
+        ],
+        temperature: 0.7,
+        model: 'gpt-3.5-turbo',
+      })
+      console.log(`${message.author.displayName}: ${chatResponse.usage.total_tokens} total tokens`)
 
-        console.log(`${message.author.displayName}: ${chatResponse.usage.total_tokens} total tokens`)
+      message.reference
+        ? (await message.fetchReference()).reply(chatResponse.choices[0].message.content)
+        : message.reply(chatResponse.choices[0].message.content)
 
-        originalMessage.reply(chatResponse.choices[0].message.content)
-      } else {
-        const chatResponse = await openai.chat.completions.create({
-          messages: [
-            {
-              role: 'system',
-              content: 'Je veux que tu fasse des réponses de moins de 100 mots.'
-            },
-            {
-              role: 'user',
-              content: message.content.replace(`<@${client.user.id}`, '')
-            }
-          ],
-          temperature: 0.7,
-          model: 'gpt-3.5-turbo'
-        })
-
-        console.log(`${message.author.displayName}: ${chatResponse.usage.total_tokens} total tokens`)
-
-        message.reply(chatResponse.choices[0].message.content)
-      }
     } catch (error) {
       appendFile('errors.log', JSON.stringify(error), err => {
         if (err) throw err
