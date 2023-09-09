7548842271ce34941bd48001f98ac9ef8f2e6c95
Nicolas Broca <n.broca@gmail.com>
2023-09-10 09:58:48 +1000

    add formatting and total cost

--- src/commands/usage.ts
+++ src/commands/usage.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 
 import { BotCommand } from '../types'
 import { db } from "../data/database";
-import { formatBigNumber } from "../helpers/format-big-number";
+import { formatBigNumber, formatDollarAmount } from "../helpers/format-big-number";
 
 const INPUT_TOKEN_PRICE_PER_THOUSAND = 0.0015;
 const OUTPUT_TOKEN_PRICE_PER_THOUSAND = 0.002;
@@ -15,22 +15,23 @@ const usageCommand: BotCommand = {
     const usageCounts = db.prompt.countUsageTokens()
 
     if (
-        null === usageCounts ||
-        undefined === usageCounts?.outputTokenCount ||
-        undefined === usageCounts?.inputTokenCount ||
-        undefined === usageCounts?.promptCount
+      null === usageCounts ||
+      undefined === usageCounts?.outputTokenCount ||
+      undefined === usageCounts?.inputTokenCount ||
+      undefined === usageCounts?.promptCount
     ) {
-        await interaction.reply(`Désolé, il y a eu un problème avec la requête, contactez Nico.`)
-        return;
+      await interaction.reply(`Désolé, il y a eu un problème avec la requête, contactez Nico.`)
+      return;
     }
-    
+
     const inputPrice = INPUT_TOKEN_PRICE_PER_THOUSAND * (usageCounts?.inputTokenCount / 1000);
     const outputPrice = OUTPUT_TOKEN_PRICE_PER_THOUSAND * (usageCounts?.outputTokenCount / 1000);
     const reply: string[] = []
 
     reply.push(`- ${formatBigNumber(usageCounts?.promptCount)} prompts envoyés`)
-    reply.push(`- ${usageCounts?.inputTokenCount} input tokens, soit ${inputPrice}$`)
-    reply.push(`- ${usageCounts?.outputTokenCount} output tokens, soit ${outputPrice}$`)
+    reply.push(`- ${formatBigNumber(usageCounts?.inputTokenCount)} input tokens, soit ${formatDollarAmount(inputPrice)}`)
+    reply.push(`- ${formatBigNumber(usageCounts?.outputTokenCount)} output tokens, soit ${formatDollarAmount(outputPrice)}`)
+    reply.push(`Soit un total de ${formatDollarAmount(inputPrice + outputPrice)}`)
 
     await interaction.reply('Les statistiques API du bot sont :\n' + reply.join('\n'))
   }
