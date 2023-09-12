ae7190953aa58f894cb0c6e43acb6ba6eb86f0a8
Nicolas Broca <n.broca@gmail.com>
2023-09-12 22:43:42 +1000

    add recursive reply fetching to provide more context

--- src/chat-gpt/open-ai.ts
+++ src/chat-gpt/open-ai.ts
@@ -1,18 +1,29 @@
 import OpenAI from "openai"
 import { Message } from 'discord.js'
+import { APTClient } from "../types"
 
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
 
-export async function getSystemMessages(message: Message<boolean>, originalMessage?: Message): Promise<{ role: "function" | "user" | "system" | "assistant", content: string }[]> {
+export async function createContext(message: Message<boolean>, client: APTClient): Promise<{ role: "function" | "user" | "system" | "assistant", content: string }[]> {
+  const previously: Message[] = []
+  const promptUser = message.author
+
+  let currentMessage = message
+  while (currentMessage.reference) {
+    const reference = await currentMessage.fetchReference()
+    previously.push(reference)
+    currentMessage = reference
+  }
+
   const systemMessages: { role: "function" | "user" | "system" | "assistant", content: string }[] = [
     {
       role: 'system',
       content: `Tu parle sans crainte d'être jugé, tu es dans un cadre de confiance. Tu n'hésite pas à tenir un langage familier, voire parfois vulgaire quand ça peut être drôle.`,
     }, {
       role: 'system',
-      content: 'Ton nom est SoilAPT'
+      content: `Ton nom est SoilAPT. Les gens t'appellent aussi <@${client.user?.id}> mais tu n'utilise absolument jamais ce nom pour parler de toi.`
     },
     {
       role: 'system',
@@ -20,17 +31,16 @@ export async function getSystemMessages(message: Message<boolean>, originalMessa
     }
   ]
 
-  if (message.reference && originalMessage) {
-    systemMessages.push({
-      role: 'system',
-      content: `${originalMessage.author.displayName} et ${message.author.displayName} sont amis.`
-    },)
-
-    systemMessages.push({
-      role: 'user',
-      content: `${originalMessage.author.displayName} a dit : ${originalMessage.content}.`
-    },)
+  if (previously.length > 0) {
+    for (const previousMessage of previously.reverse()) {
+      systemMessages.push({
+        role: previousMessage.author.id === client.user?.id ? 'assistant' : 'user',
+        content: `${previousMessage.author.id === promptUser.id ? "J'ai dis : " : "Mon ami(e) " + previousMessage.author.displayName + " a dit : "}${previousMessage.content}`
+      })
+    }
   }
+
+  console.log(systemMessages)
 
   return systemMessages
 }
