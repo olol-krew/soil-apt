179e7bae3a7da68a6387d172607290aebfb1de4f
Nicolas Broca <n.broca@gmail.com>
2023-09-16 08:41:47 +1000

    refactor potd loading make it error resistant change expiry method

--- src/helpers/pick-new-potd.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "../data/database";
-
-export default function pickNewPotd() {
-  const newPersona = db.persona.getOneRandomly()
-  db.potd.create(newPersona)
-  return db.potd.getMostRecent()
-}
