71c944f2d5a29afdfa57f5273d0ee74ab41483e3
Nicolas Broca <n.broca@gmail.com>
2023-09-24 22:05:46 +1000

    fix auth user table variable

--- api/routes/auth-users.ts
+++ api/routes/auth-users.ts
@@ -4,17 +4,17 @@ import { db } from "../data/database";
 const userRouter = Router()
 
 userRouter.get('/', (req, res) => {
-  res.json(db.user.getAll())
+  res.json(db.authUser.getAll())
 })
 
 userRouter.get('/:id', (req, res) => {
-  const user = db.user.get(req.params.id)
+  const user = db.authUser.get(req.params.id)
   if (!user) return res.sendStatus(404)
   return res.json(user)
 })
 
 userRouter.delete('/:id', (req, res) => {
-  db.user.delete(req.params.id)
+  db.authUser.delete(req.params.id)
   return res.sendStatus(204)
 })
 
