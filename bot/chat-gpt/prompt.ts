2093c5fd1115411a7b3389d9a3ab91026306cc3c
Pierre Maraitre <balamung@protonmail.com>
2023-11-08 09:37:56 +0100

    Update prompt.ts
    Change for GPT-4 turbo to save on pricing

--- bot/chat-gpt/prompt.ts
+++ bot/chat-gpt/prompt.ts
@@ -41,7 +41,7 @@ export async function handlePrompt(client: APTClient, message: Message) {
         }
       ],
       temperature: 0.7,
-      model: 'gpt-4',
+      model: 'gpt-4-1106-preview',
     })
 
     const promptParams = {
