9f07b2a75198659acf72b297d0f6811cc9d6c72c
Nicolas Broca <n.broca@gmail.com>
2024-02-18 13:36:07 +1100

    remove prompt tracking

--- bot/chat-gpt/prompt.ts
+++ bot/chat-gpt/prompt.ts
@@ -5,8 +5,6 @@ import { createContext, openai } from "./open-ai";
 import { log } from "../../common/helpers/logger";
 import { Persona } from "../../api/data/persona";
 import fetchApi from "../helpers/fetch-api";
-import { Prompt } from "../../api/data/prompt";
-import { PromptUser } from "../../api/data/prompt-user";
 
 export async function handlePrompt(client: APTClient, message: Message) {
   if (!message.content.startsWith(`<@${client.user?.id}>`))
@@ -14,22 +12,9 @@ export async function handlePrompt(client: APTClient, message: Message) {
 
   log.info(`Prompt received from ${message.author.displayName}.`)
 
-  const [persona, dbUser] = await Promise.all([
-    fetchApi<Persona>('/api/personas/today'),
-    fetchApi<PromptUser>(`/api/prompt-users/${message.author.id}`)
-  ])
+  const persona = await fetchApi<Persona>('/api/personas/current')
 
   if (!persona) return
-  if (!dbUser) {
-    await fetchApi<PromptUser>('/api/prompt-users', {
-      method: "POST",
-      body: JSON.stringify({
-        id: message.author.id,
-        name: message.author.username,
-        avatar: message.author.avatar
-      })
-    })
-  }
 
   try {
     const chatResponse = await openai.chat.completions.create({
@@ -44,27 +29,7 @@ export async function handlePrompt(client: APTClient, message: Message) {
       model: 'gpt-4-1106-preview',
     })
 
-    const promptParams = {
-      userId: message.author.id,
-      isResponse: message.reference !== null,
-      responseTo: message.reference && (await message.fetchReference()).content,
-      inputToken: chatResponse.usage?.prompt_tokens,
-      outputToken: chatResponse.usage?.completion_tokens,
-      input: message.content,
-      output: chatResponse.choices[0].message.content
-    }
-
-    const prompt = await fetchApi<Prompt>('/api/prompts/', {
-      method: "POST",
-      body: JSON.stringify(promptParams)
-    })
-
-    if (!prompt) {
-      log.error(`There was an issue when creating the prompt for the DB.`)
-      return
-    }
-
-    log.info(`Response from OpenAI received. Input token: ${prompt.inputToken}, output token: ${prompt.outputToken}. Replying...`)
+    log.info(`Response from OpenAI received. Input token: ${chatResponse.usage?.prompt_tokens}, output token: ${chatResponse.usage?.completion_tokens}. Replying...`)
     message.reply(chatResponse.choices[0].message.content || `Désolé, il y a eu un problème avec la requête, contactez Nico.`)
   } catch (error) {
     log.error(error)
