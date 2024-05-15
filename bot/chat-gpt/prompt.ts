30474d0595d2b00c882595a6da4a44c8d2303d35
Nicolas Broca <n.broca@gmail.com>
2024-05-15 21:33:28 +1000

    refactor prompt command to consume open ai class

--- bot/chat-gpt/prompt.ts
+++ bot/chat-gpt/prompt.ts
@@ -1,14 +1,14 @@
-import { Message } from "discord.js";
+import { Message } from 'discord.js'
+import type { ChatMessage } from '../types'
 
-import type { APTClient } from "../types";
-import { createContext, openai } from "./open-ai";
+import type { APTClient } from '../types'
+import { openai } from './open-ai'
 import { log } from 'kabum-ts-logger'
-import type { Persona } from "../../api/data/persona";
-import fetchApi from "../helpers/fetch-api";
+import type { Persona } from '../../api/database/persona'
+import fetchApi from '../helpers/fetch-api'
 
 export async function handlePrompt(client: APTClient, message: Message) {
-  if (!message.content.startsWith(`<@${client.user?.id}>`))
-    return
+  if (!message.content.startsWith(`<@${client.user?.id}>`)) return
 
   log.info(`Prompt received from ${message.author.displayName}.`)
 
@@ -17,20 +17,19 @@ export async function handlePrompt(client: APTClient, message: Message) {
   if (!persona) return
 
   try {
-    const chatResponse = await openai.chat.completions.create({
-      messages: [
-        ...await createContext(message, client, persona),
-        {
-          role: 'user',
-          content: message.content.replace(`<@${client.user?.id}> `, '')
-        }
-      ],
-      temperature: 0.8,
-      model: 'gpt-4o',
-    })
+    const chatResponse = await openai.createChatCompletion(
+      message,
+      client,
+      persona
+    )
 
-    log.info(`Response from OpenAI received. Input token: ${chatResponse.usage?.prompt_tokens}, output token: ${chatResponse.usage?.completion_tokens}. Replying...`)
-    message.reply(chatResponse.choices[0].message.content || `Désolé, il y a eu un problème avec la requête, contactez Nico.`)
+    log.info(
+      `Response from OpenAI received. Input token: ${chatResponse.usage?.prompt_tokens}, output token: ${chatResponse.usage?.completion_tokens}. Replying...`
+    )
+    message.reply(
+      chatResponse.choices[0].message.content ||
+        `Désolé, il y a eu un problème avec la requête, contactez Nico.`
+    )
   } catch (error) {
     log.error(error)
   }
