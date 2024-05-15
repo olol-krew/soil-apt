6315211c27f83d4712c6a93d07b47a78c8d1f175
Nicolas Broca <n.broca@gmail.com>
2024-05-15 21:33:39 +1000

    add types

--- bot/types.ts
+++ bot/types.ts
@@ -8,3 +8,17 @@ export interface BotCommand {
 export interface APTClient extends Client {
   commands?: Collection<string, BotCommand>
 }
+
+export interface ChatMessageContent {
+  type: 'text' | 'image_url'
+  text?: string
+  image_url?: {
+    url: string
+  }
+}
+
+export interface ChatMessage {
+  role: 'user' | 'system' | 'assistant'
+  content: ChatMessageContent[]
+}
+
